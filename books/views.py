aff8920f5cdeab74b0a62ad12fb08ed8554c3246
SNikolaY <sny_85@mail.ru>
2023-05-05 23:07:53 +0400

    initial commit

--- books/views.py
+++ books/views.py
@@ -1,61 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.core.paginator import Paginator
 from books.models import Book
 
 
-def forming(books_objects):
-    books = []
-    for book in books_objects:
-        book_dict = {
-            'name': book.name,
-            'author': book.author,
-            'pub_date': book.pub_date,
-        }
-        books.append(book_dict)
-    return books
-
-
-def forming_date(books_objects):
-    books = []
-    for book in books_objects:
-        book_dict = {
-            str(book.pub_date):{
-                'name': book.name,
-                'author': book.author,
-            }
-        }
-        books.append(book_dict)
-    return books
-# content = [
-#     {'pub_date':{'name': book.name, 'author': book.author}}
-# ]
-
 def books_view_list(request):
     books_objects = Book.objects.all()
-    books = forming(books_objects)
     template = 'books/books_list.html'
     context = {
-        'books': books
+        'books': books_objects
     }
     return render(request, template, context)
 
 
 def books_page(request, pub_date):
-    page_number = request.path
-    #books_objects = Book.objects.filter(pub_date=pub_date)
-    books_objects = Book.objects.all()
-    paginator = Paginator(forming_date(books_objects), 5)
-    page = paginator.get_page(page_number)
-    previous_page = 30
-    next_page = 50
-    print(page_number)
-    print(forming_date(books_objects))
-    template = 'books/books.html'
+    books_objects = Book.objects.filter(pub_date__exact=pub_date)
+    previous_page = Book.objects.filter(pub_date__lt=pub_date).first()
+    next_page = Book.objects.filter(pub_date__gt=pub_date).first()
+    template = 'books/books_list.html'
     context = {
-        'books': page,#forming_date(books_objects),
-        'pub_date': pub_date,
-        'page': page,
+        'books': books_objects,
         'previous_page': previous_page,
         'next_page': next_page
     }
